022d28db38703e5b02b4975e81eb0c4dc06bc54b
luan- <luan-@LAPTOP-RELJI6IG>
2022-02-09 11:31:13 -0300

    subindo o primeiro projeto

---
+++ Exemplo2SIF/src/Exemplo1.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Exemplo1 {
+
+	public static void main(String[] args) {
+		
+		int[] n = new int[5]; // local
+		lerDados(n); // argumento -> será enviada para o método
+		int maior = maiorValor(n);
+		int menor = menorValor(n);
+		System.out.println("maior valor = "+ maior);
+		System.out.println("menor valor = "+ menor);
+
+	}
+	
+	public static int maiorValor(int[] n) { //metodo com retorno
+		int aux = n[0];//inicializa com um valor que já tá la dentro
+		for(int i=0 ; i< n.length;i++) {
+			if(n[i] > aux) {
+				aux = n[i];
+			}
+		}
+		return aux;
+	}
+	
+	
+	public static int menorValor(int[] n) {
+		int aux = n[0];//inicializa com um valor que já tá la dentro
+		for(int i=0 ; i< n.length;i++) {
+			if(n[i] < aux) {
+				aux = n[i];
+			}
+		}
+		return aux;
+		
+		
+		
+	}
+	
+	
+
+	public static void lerDados(int[] n) { // metodo sem retorno - parametro -> variavel que recebe o valor
+		Scanner teclado = new Scanner(System.in);
+		for(int i=0;i<n.length;i++) {
+			System.out.print("Valor: ");
+			n[i] = teclado.nextInt();
+		}
+		teclado.close();
+	}
+	
+	
+}
